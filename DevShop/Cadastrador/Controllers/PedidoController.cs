7ac0f42687aabb3babbe4ad5ebd3f674fc3a5c46
Jefferson <jeff.debian2016@gmail.com>
2022-12-28 23:56:13 -0300

    Inserindo pedido no dynamoDb.

---
+++ DevShop/Cadastrador/Controllers/PedidoController.cs
@@ -0,0 +1,21 @@
+﻿using Compartilhado.Model;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+
+namespace Cadastrador.Controllers
+{
+    [Route("api/[controller]")]
+    public class PedidoController : ControllerBase
+    {
+        [HttpPost]
+        public async Task PostAsync([FromBody] Pedido pedido)
+        {
+            pedido.Id = Guid.NewGuid().ToString();
+            pedido.DataDeCriacao = DateTime.Now;
+
+            await pedido.SalvarAsync();
+
+            Console.WriteLine($"Pedido salvo com sucesso: id {pedido.Id}");
+        }
+    }
+}
